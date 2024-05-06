6764ea90e3f59daf8b755aab95d6b5b623fd58a2
zeroqs <smthq@bk.ru>
2024-05-06 18:28:59 +0300

    feat(canvas): add icon to canvas

--- src/app/layer/[slug]/page.tsx
+++ src/app/layer/[slug]/page.tsx
@@ -1,11 +1,29 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+'use client'
 
+import Image, { StaticImageData } from 'next/image'
+import { useState } from 'react'
+
+import icon from '@/../public/infantry.png'
+import { Button } from '@/shared/ui/button'
 import { Map } from '@/widgets/layer/Map'
 
 export default function Layer({ params }: { params: { slug: string } }) {
+  const [selectedIcon, setSelectedIcon] = useState<StaticImageData | null>(null)
+
   return (
-    <div>
-      <Map />
+    <div className="flex justify-between">
+      <Map selectedIcon={selectedIcon} />
+      <div className="p-4">
+        <Button
+          onClick={() => setSelectedIcon(icon)}
+          variant="outline"
+          size="icon"
+          className="p-2"
+        >
+          <Image width={30} height={30} src={icon} alt="infantry" />
+        </Button>
+      </div>
     </div>
   )
 }
--- src/widgets/layer/Map.tsx
+++ src/widgets/layer/Map.tsx
@@ -1,13 +1,24 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { StaticImageData } from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 
 import layer from '@/../public/FoolsRoad.webp'
 
-export const Map = () => {
+interface MapProps {
+  selectedIcon: StaticImageData | null
+}
+
+export const Map = ({ selectedIcon }: MapProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [clickedPoint, setClickedPoint] = useState<{
+    x: number
+    y: number
+  } | null>(null)
+  const [icons, setIcons] = useState<{ x: number; y: number }[]>([])
+  const [movingIconIndex, setMovingIconIndex] = useState<number | null>(null)
 
   useEffect(() => {
     // define a custom handler function
@@ -45,6 +56,55 @@ export const Map = () => {
       context!.drawImage(image, 0, 0, canvas.width, canvas.height)
     })
   }, [])
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas || !selectedIcon || !clickedPoint) return
+
+    const context = canvas.getContext('2d')
+    const iconImage = new Image()
+    iconImage.src = selectedIcon.src
+    iconImage.addEventListener('load', () => {
+      context!.drawImage(
+        iconImage,
+        clickedPoint.x - 20,
+        clickedPoint.y - 20,
+        30,
+        30,
+      )
+    })
+  }, [selectedIcon, clickedPoint, icons, movingIconIndex])
+
+  const handleCanvasClick = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current
+    if (!canvas || !selectedIcon) return
+
+    const rect = canvas.getBoundingClientRect()
+    const scaleX = canvas.width / rect.width
+    const scaleY = canvas.height / rect.height
+    const offsetX = rect.left
+    const offsetY = rect.top
+    const x = (e.clientX - offsetX) * scaleX
+    const y = (e.clientY - offsetY) * scaleY
+
+    // Проверяем, попал ли клик внутрь какой-либо из существующих иконок
+    const iconIndex = icons.findIndex((icon) => {
+      return (
+        x >= icon.x - 15 &&
+        x <= icon.x + 15 &&
+        y >= icon.y - 15 &&
+        y <= icon.y + 15
+      )
+    })
+
+    if (iconIndex !== -1) {
+      setMovingIconIndex(iconIndex)
+    } else {
+      setClickedPoint({ x, y })
+      setIcons([...icons, { x, y }])
+    }
+  }
+
   return (
     <TransformWrapper>
       <TransformComponent>
@@ -57,7 +117,11 @@ export const Map = () => {
             }
           }}
         >
-          <canvas className="rounded-2xl" ref={canvasRef} />
+          <canvas
+            className="rounded-2xl"
+            ref={canvasRef}
+            onClick={handleCanvasClick}
+          />
         </div>
       </TransformComponent>
     </TransformWrapper>
