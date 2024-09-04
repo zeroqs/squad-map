c50c5b7432fbb4494a175626ae51f818c1ceed9f
zeroqs <smthq@bk.ru>
2024-09-04 15:05:11 +0300

    ☄️ fix: del unused const

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,5 +1,3 @@
 import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient()
-export const fetcher = (...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((res) => res.json())
