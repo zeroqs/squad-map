9fbf10e937942213bd46fd4c11e91a5adf13c763
zeroqs <smthq@bk.ru>
2024-09-19 00:58:09 +0300

    ☄️ fix: api url add

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   eslint: {
       ignoreDuringBuilds: true,
   },
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://squad-map.vercel.app/:path*',
-      },
-    ]
-  },
   images: {
     remotePatterns: [
       {
